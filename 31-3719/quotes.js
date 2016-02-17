5e92c4af20d9c300f56a68bcf41a93af3eb27c8d
joe3141 <youssefzidan.yz@gmail.com>
2016-02-17 21:35:57 +0200

    Added getting a single quote or all quotes from the json file

--- 31-3719/quotes.js
+++ 31-3719/quotes.js
@@ -1,6 +1,16 @@
+var ob = require("../quotes.json")
 
 function getElementByIndexElseRandom (arr, index) {
-	 index = index === undefined ? Math.floor(Math.random() * array.length) : index
+	 index = index === undefined ? Math.floor(Math.random() * arr.length) : index
 	 return arr[index]
 }
+
+function getQuotesFromJSON() {
+	return ob	
+}
+
+function getQuoteFromJSON(index) {
+	return getElementByIndexElseRandom(getQuotesFromJSON(), index)
+}
+
 
