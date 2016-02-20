dc2038f9e21f5a3a41367908505b4bc7e2b198c7
joe3141 <youssefzidan.yz@gmail.com>
2016-02-20 22:41:29 +0200

    Seeding and clearing the database

---
+++ 31-3719/db.js
@@ -0,0 +1,37 @@
+var mongo = require('mongodb')
+var mongoC = mongo.MongoClient
+
+var url = 'mongodb://localhost:27017/se_1'
+var _db
+
+function connect(cb) {
+	mongo.connect(url, function(err, db) {
+ 			if(err)
+ 				throw err
+
+ 			_db = db
+ 			cb(db)
+ 		})
+}
+
+function db() {
+	return _db
+}
+
+function clear_db(cb) {
+	_db.collection('quotes').deleteMany({}, function(err, res) {
+		console.log(res)
+		cb()
+	})
+}
+
+function close(cb){
+	_db.close()
+	cb()
+}
+
+exports.db = db;
+exports.connect = connect
+exports.close = close
+
+
--- 31-3719/quotes.js
+++ 31-3719/quotes.js
@@ -1,4 +1,8 @@
 var ob = require("../quotes.json")
+var db = require("./db.js")
+
+var _db 
+db.connect(function(dbo) {_db = dbo})
 
 function getElementByIndexElseRandom (arr, index) {
 	 index = index === undefined ? Math.floor(Math.random() * arr.length) : index
@@ -14,3 +18,29 @@ function getQuoteFromJSON(index) {
 }
 
 
+function seed(cb) {
+	var collection = _db.collection("quotes")
+	collection.count(function(err, c) {
+		if(err)
+			cb(err, null)
+			throw err
+		if(c === 0){
+			ob.foreach(function(q) {
+				collection.insertOne(q, function(err, res) {
+					if(err)
+						throw err
+
+				})
+			})
+			cb(null, true)
+		}else{
+			cb(null, false)
+		}
+	})
+}
+
+
+exports.getElementByIndexElseRandom = getElementByIndexElseRandom
+exports.getQuotesFromJSON = getQuotesFromJSON
+exports.getQuoteFromJSON = getQuoteFromJSON
+exports.seed = seed
