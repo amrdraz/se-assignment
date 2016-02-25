c8c62510cbcd95a5e553a0bedd291ab4801f1cc6
2shraaf <ahmed.awwad.me@gmail.com>
2016-02-25 03:50:54 +0200

    catch 404

--- 31-14959/app.js
+++ 31-14959/app.js
@@ -40,43 +40,10 @@ app.get('/api/quote' , function (req,res ){
 
 
 
+/// catch 404 and forwarding to error handler
+app.use(function(req, res, next) {
+     res.send('404: Page not Found', 404);
+     // next(err);
+});
 
-
-
-// var router = express.Router();
-// router.get('/', function(req, res, next) {
-//   res.send('respond with a resource');
-//
-// });
-// /// catch 404 and forwarding to error handler
-// app.use(function(req, res, next) {
-//     var err = new Error('Not Found');
-//     err.status = 404;
-//     next(err);
-// });
-//
-// /// error handlers
-//
-// // development error handler
-// // will print stacktrace
-// if (app.get('env') === 'development') {
-//     app.use(function(err, req, res, next) {
-//         res.status(err.status || 500);
-//         res.render('error', {
-//             message: err.message,
-//             error: err
-//         });
-//     });
-// }
-//
-// // production error handler
-// // no stacktraces leaked to user
-// app.use(function(err, req, res, next) {
-//     res.status(err.status || 500);
-//     res.render('error', {
-//         message: err.message,
-//         error: {}
-//     });
-// });
-//
 module.exports = app;
