da60560b73e7c778f021609b8d65d01ccc10d5dd
EsraaaSalah <esraasalah171995@gmail.com>
2016-02-26 22:46:22 +0200

    seed done

--- 31-6542/quotes.js
+++ 31-6542/quotes.js
@@ -1,5 +1,9 @@
 var fs = require('fs');
-
+var dbFile= require('./db.js');
+var allQuotes = fs.readFileSync("../quotes.json");
+dbFile.connect();
+var db = dbFile.db();
+//var app=require('./app');
 var getElementByIndexElseRandom = function getElementByIndexElseRandom(array , index)
 {
 		if(index === undefined)
@@ -16,7 +20,7 @@ var getElementByIndexElseRandom = function getElementByIndexElseRandom(array , i
 
 var getQuotesFromJSON = function getQuotesFromJSON()
 {
-	var allQuotes = fs.readFileSync("quotes.json");
+	 
 	return JSON.parse(allQuotes);
 }
 
@@ -25,4 +29,33 @@ var getQuoteFromJSON = function getQuoteFromJSON(index)
 	var retrievedQuotes = getQuotesFromJSON();
 	return getElementByIndexElseRandom(retrievedQuotes, index);
 }
-console.log(getQuoteFromJSON());
+
+var seed = function seed(cb)
+{
+	//var db = express.db;
+	
+	db.myQuotes.insert()(JSON.parse(allQuotes))
+
+}
+seed(null);
+	/*
+	var myQuotes = db.get('Quotes');
+	/*
+	console.log('hi'+myQuotes.find());
+	myQuotes.insert({ "username" : "testuser1", "email" : "testuser1@testdomain.com" });
+	console.log(myQuotes.find().pretty());
+}
+/*
+var getQuotesFromDB = getQuotesFromDB(cb) 
+{
+
+}
+var getQuoteFromDB = getQuoteFromDB(cb [, index])
+{
+
+}*/
+exports.getElementByIndexElseRandom = getElementByIndexElseRandom;
+exports.getQuotesFromJSON = getQuotesFromJSON;
+exports.getQuoteFromJSON = getQuoteFromJSON;
+//seed(null);
+//console.log(getQuoteFromJSON());*/
