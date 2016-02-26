8748e3a5512ad4ae8b6dc1dfb111976f44c03441
Narihan <narihanellaithy@gmail.com>
2016-02-26 22:23:47 +0200

    Done but bugs
    Making the directories, css,html yet potential problems in main.js,
    quotes.js, server.js, app.js, and db.js.

---
+++ 31-8027/public/js/main.js
@@ -0,0 +1,88 @@
+
+
+var $authors= $('.authors'); //modified more than once
+var $quotes= $('.quotes');
+
+$('.div').on('click', function (event) {
+    $.ajax({
+        url: 'api/quotes';
+        //'../../quotes.json',
+        success: function (post) {
+            $authors.html(quotes.author);
+            $quotes.html(quotes.text);
+        }
+    });
+});
+ /*$(function(){
+    $.ajax({
+      url: '/api/quote',
+      type: 'GET',
+      dataType: 'json',
+      
+      success: function (data) {
+        console.log('success', data);
+      },
+    });
+  });
+*/
+
+$(document).ready(function(){
+	$(".background").click(function(){
+			 $(this).css("background",randomColor());
+	});
+
+});
+
+
+var safeColors = ["00","33","66","99","cc","ff"];
+var rand = function() {
+    return Math.floor(Math.random()*6);
+};
+var randomColor = function() {
+    var r = safeColors[rand()];
+    var g = safeColors[rand()];
+    var b = safeColors[rand()];
+    return "#"+r+g+b;
+};
+
+
+
+/*$(document).ready(function()
+{
+ 
+function toggle_color(color1,color2,cycle_time,wait_time) {
+setInterval(function first_color() {
+    $("body").animate({
+        backgroundColor: color1
+    }, 1000, function () {
+        setTimeout(change_color, wait_time);
+    });
+}, cycle_time);
+ 
+function change_color() {
+    $("body").animate({
+        backgroundColor: color2
+    }, 1000, function () {
+        setTimeout(function () {}, wait_time);
+    });
+}
+}
+ 
+toggle_color('#61beb3','#7d4b68',8000,3000);
+});*/
+
+/*fetch('quotes.json').then(function(res){
+	return res.json();
+}).then(function(quotes){
+	document.body.addEventListener('click',function(){
+		var quote=""; //add a way to get quotes
+		document.body.style.backgroundColor='rgb('+(Math.random()*255)+','+(Math.random()*255)+','+(Math.random()*255)+''
+
+	})
+})
+*/
+
+
+
+
+}
