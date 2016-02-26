a1d7a7e85ef6a34e7657e5b54cb20d9fdd5bb05f
EsraaaSalah <esraasalah171995@gmail.com>
2016-02-26 14:55:24 +0200

    Second function done

--- 31-6542/quotes.js
+++ 31-6542/quotes.js
@@ -1,4 +1,4 @@
-
+var fs = require('fs');
 
 var getElementByIndexElseRandom = function getElementByIndexElseRandom(array , index)
 {
@@ -13,7 +13,11 @@ var getElementByIndexElseRandom = function getElementByIndexElseRandom(array , i
     		return array[index];
 }
 
-var x = getElementByIndexElseRandom([1, 3, 4], 0) ;
-var y = getElementByIndexElseRandom([1, 3, 4]) ;
-console.log(x);
-console.log(y);
+
+var getQuotesFromJSON = function getQuotesFromJSON()
+{
+	var allQuotes = fs.readFileSync("quotes.json");
+	return JSON.parse(allQuotes);
+}
+var q = getQuotesFromJSON();
+console.log(q);
