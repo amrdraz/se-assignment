ca216cf1f6f7aa5e85702d065c73742933465631
xXGenomXx <mostafa.ebrahim@hotmail.com>
2016-02-29 00:32:05 +0200

    still need update

---
+++ 29-14677/public/javascripts/main.js
@@ -0,0 +1,19 @@
+
+    var $quote = document.querySelector('.quote');
+    var $author = document.querySelector('.author');
+
+
+    function getElementByIndexElseRandom(array, index) {
+        index = index === undefined ? Math.floor(Math.random() * array.length) : index;
+        return array[index];
+    }
+    fetch('./quotes.json').then(function(res) {
+        return res.json();
+    }).then(function(quotes) {
+        document.body.addEventListener('click', function() {
+            var quote = getElementByIndexElseRandom(quotes);
+            document.body.style.backgroundColor = 'hsl(' + (Math.random() * 360) + ', 55%, 80%)'
+            $quote.innerHTML = quote.text;
+            $author.innerHTML = quote.author;
+        });
+    });
