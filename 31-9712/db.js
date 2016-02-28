0f932fb6fb56ad4bfbd92821125dd34c86c9ec90
Hossam <hossam.elfar95@gmail.com>
2016-02-28 03:50:01 +0200

    Fixes A lot of errors

--- 31-9712/db.js
+++ 31-9712/db.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //importing mongoDB api
 var mongo = require('mongodb');
 var mongoC = mongo.MongoClient;
@@ -10,14 +9,6 @@ var DB=null;
 
 //Function to connect To DB
 // if connected set DB to this db
-=======
-var mongo = require('mongodb')
-var mongoC = mongo.MongoClient
-
-var url = 'mongodb://localhost:27017/quotes'
-var DB=null;
-
->>>>>>> 5ae9d3b5aa6d8da6df309fc15e473dd8684cb91b
 function connect(cb) {
     if(DB) cb(null)
     mongo.connect(url, function(err, db) {
@@ -28,7 +19,6 @@ function connect(cb) {
     })
 }
 
-<<<<<<< HEAD
 //just a getter function
 function db() {
     //This condition is just for ensuring that we will not return a NULL var
@@ -45,36 +35,19 @@ function clearDB(cb) {
     DB.collection('quotes').remove({}, function(err, res) {
         if(err)
             throw err;
-=======
-function db() {
-    if(DB === null) throw Error('DB Object has not yet been initialized')
-    return DB
-}
-
-function clearDB(cb) {
-    DB.collection('quotes').deleteMany({}, function(err, res) {
-        if(err)
-            throw err
->>>>>>> 5ae9d3b5aa6d8da6df309fc15e473dd8684cb91b
         cb()
     })
 }
 
 
-<<<<<<< HEAD
 // a function to close the connection of our DB
-=======
->>>>>>> 5ae9d3b5aa6d8da6df309fc15e473dd8684cb91b
 function close(cb){
     if(DB)
         DB.close()
     cb()
 }
 
-<<<<<<< HEAD
 //exporting stuff
-=======
->>>>>>> 5ae9d3b5aa6d8da6df309fc15e473dd8684cb91b
 exports.db = db;
 exports.connect = connect;
 exports.close = close;
