7a8c102e4a1bb6bcbb84853d32f98a9f54819328
AhmadElsagheer <ahmad.elsagheer@yahoo.com>
2016-03-03 10:41:42 +0200

    created API

--- 31-15449/routes.js
+++ 31-15449/routes.js
@@ -6,4 +6,20 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/index', function(req, res) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/api/quote', function(req, res) {
+  quotes.getQuoteFromDB(function(err, quote){
+    res.json(quote);
+  });
+});
+
+router.get('/api/quotes', function(req, res) {
+  quotes.getQuotesFromDB(function(err, quotes){
+    res.json(quotes);
+  });
+});
+
 module.exports = router;
