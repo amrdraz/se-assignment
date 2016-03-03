5d856273b82268e0792df59ed99e9a59fc590eed
NoorKindy <noorelkindy@gmail.com>
2016-03-03 21:12:04 +0200

    done

---
+++ 31-9103/public/js/main.js
@@ -0,0 +1,30 @@
+var safeColors = ['00','33','66','99','cc','ff'];
+var rand = function() {
+    return Math.floor(Math.random()*6);
+};
+var randomColor = function() {
+    var r = safeColors[rand()];
+    var g = safeColors[rand()];
+    var b = safeColors[rand()];
+    return "#"+r+g+b;
+};
+$(document).ready(function() {
+    $('html').click(function() {
+        $('div').each(function() {
+            $(this).css('background-color',randomColor());
+
+        });
+    });
+});
+$(document).click(function (){
+	$.ajax({
+	    url: "/api/quote",
+	    success: function(res){
+	      $('.post-list-item-header').html(res.text);
+	      $('.author').html(res.author);
+
+	    }
+	});
+
+	$('header').css('background-color',randomColor());
+})
---
+++ 31-9103/quotes.js
@@ -0,0 +1,53 @@
+var db = require('./db.js');
+exports.getElementByIndexElseRandom= function getElementByIndexElseRandom(array , index){  
+    if(index===undefined){
+      var r = Math.floor(Math.random()*array.length);
+      var x = array[r];
+        
+    }else{
+        var x = array[index];
+    }
+    return x;
+}
+exports.getQuotesFromJSON= function getQuotesFromJSON(){
+    var x = require('./quotes.json');
+    return x; 
+}
+exports.getQuoteFromJSON= function getQuoteFromJSON(index){
+    var x = this.getQuotesFromJSON();
+    if(index===undefined){
+        var y = this.getElementByIndexElseRandom(x);
+    }else{
+        var y = this.getElementByIndexElseRandom(x, index);
+    }
+    return y;
+}
+exports.seed =function seed(cb) {
+    var x = this.getQuotesFromJSON();  
+    db.db().collection('quotes').count(function(err, count){
+        if(count !== 0)
+          cb(err,false)
+        else
+         db.db().collection('quotes').insert(x, function(err){
+            if (err) {
+               cb(err, false); 
+            } else {
+                cb(err, true);
+            }
+        });
+    });
+}
+exports.getQuotesFromDB = function getQuotesFromDB(cb){
+  db.db().collection('quotes').find().toArray(cb);
+}
+exports.getQuoteFromDB = function getQuoteFromDB(cb, index){
+    var self = this;
+    this.getQuotesFromDB(function (err,r){
+        var x = self.getElementByIndexElseRandom(r,index);
+        cb(err, x);
+    })   
+}
+
+
+
+
