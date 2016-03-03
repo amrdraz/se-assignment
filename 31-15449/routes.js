d44f5aa52f8b8639fbf4bb988093d5e8f4e3823e
AhmadElsagheer <ahmad.elsagheer@yahoo.com>
2016-03-03 10:37:27 +0200

    created the structure and the server and configured express

---
+++ 31-15449/routes.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+var quotes = require('./quotes.js');
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
