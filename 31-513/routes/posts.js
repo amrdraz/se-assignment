0cd660398d2bbdf4f32c9894718ff9c4751df3ba
mounie <monnakhaled9@gmail.com>
2016-03-03 23:39:44 +0200

    final submission

---
+++ 31-513/routes/posts.js
@@ -0,0 +1,13 @@
+
+var express = require('express');
+var db = require('../db.js');
+var router = express.Router();
+
+router.get('/api/post', function(req, res, next) {
+    db.db().collection('post').findOne(function(err, post) {
+        if (err) return next(err);
+        res.send(post);
+    });
+});
+
+module.exports = router;
