59c868d317bc1ceea5dbdcc778b09caa3e1c2e45
Ammar Yasser <ammaryasser107@gmail.com>
2016-03-03 21:58:22 +0200

    done

---
+++ 31-2576/routes/index.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var router = express.Router();
+var quote = require('../quotes.js');
+
+// router.get('/' , function(req,res){
+// 	res.render('index');
+// });
+
+
+router.get('/api/quote',function(req,res){
+	 quote. getQuoteFromDB(function(error,quote){
+	 	if (error){
+	 		res.send("error");
+	 		throw error;
+	 	}
+	 	else	
+	 		res.json(quote);
+	 		 	
+
+
+	 },null);
+});
+
+router.get('/api/quotes',function(req,res){
+	quote.getQuotesFromDB(function(error,quote){
+		if (error){
+			throw error;
+			res.send("errorr");
+		}
+		else
+			res.json(quote);
+	});
+});
+
+	router.get('/index',function(req , res){
+			console.log("indeeeeex");
+			res.render('index.hjs');
+
+	});
+
+
+
+module.exports = router;
